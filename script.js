f4b22a6b1e20bc666a4db438d18444d27d3c266c
Lucas Peixoto <luquinhas.peixoto.sp@gmail.com>
2026-06-01 23:45:46 -0300

    quarto commit do java

--- script.js
+++ script.js
@@ -42,3 +42,23 @@ function receberQuantidadePessoas(evento) {
     }
 
     calcular()
+
+const botoesGorjeta =
+    document.querySelectorAll(".gorjeta input[type='button']")
+
+botoesGorjeta.forEach(botao => {
+
+    botao.addEventListener("click", receberPorcentagem)
+
+})
+
+function receberPorcentagem(evento) {
+
+}botoesGorjeta.forEach(botao => {
+
+        botao.classList.remove("botao-ativo")
+
+        if (botao.value === evento.target.value) {
+            botao.classList.add("botao-ativo")
+        }
+    })
